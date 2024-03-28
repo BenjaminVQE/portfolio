8b9de342427b057096cfc9e7bf98bc57aeaafe3c
Ben <vaique.benjamin@gmail.com>
2024-03-28 12:39:58 +0100

    fix(card): card click to project

--- src/components/cardProject/cardProject.tsx
+++ src/components/cardProject/cardProject.tsx
@@ -13,45 +13,45 @@ interface IconProps {
 function CardProject({ link, name, github, lang, desc, logo }: IconProps) {
   return (
     <div className="parent">
-      <div className="cardProject card">
-        <div className="backCard">
-          <span>{name}</span>
-        </div>
-        <div className="containerCard">
-          <div className="containerImgProject">
-            <img
-              src={process.env.PUBLIC_URL + logo}
-              alt={`Logo ${name}`}
-              className="cardImgProject"
-            />
+      <a href={link}>
+        <div className="cardProject card">
+          <div className="backCard">
+            <span>{name}</span>
           </div>
+          <div className="containerCard">
+            <div className="containerImgProject">
+              <img
+                src={process.env.PUBLIC_URL + logo}
+                alt={`Logo ${name}`}
+                className="cardImgProject"
+              />
+            </div>
 
-          <div className="cardBodyProject content-box">
-            <div className="card-title">
-              <a href={link}>
+            <div className="cardBodyProject content-box">
+              <div className="card-title">
                 <h3>{name}</h3>
-              </a>
-            </div>
-            <p className="card-content">
-              <span className="desc ">Descriptif :</span> <br /> {desc}
-            </p>
-            <div className="langContainer langCard">
-              {lang.map((i: any, index: number) => (
-                <span key={index} className="langCard">
-                  {i}{" "}
-                </span>
-              ))}
-            </div>
-            <div className="githubLinkCard">
-              <a href={github}>
-                <div>
-                  <FaGithub size={40} className="logoGitHub" />
-                </div>
-              </a>
+              </div>
+              <p className="card-content">
+                <span className="desc ">Descriptif :</span> <br /> {desc}
+              </p>
+              <div className="langContainer langCard">
+                {lang.map((i: any, index: number) => (
+                  <span key={index} className="langCard">
+                    {i}{" "}
+                  </span>
+                ))}
+              </div>
+              <div className="githubLinkCard">
+                <a href={github}>
+                  <div>
+                    <FaGithub size={40} className="logoGitHub" />
+                  </div>
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </a>
     </div>
   );
 }
