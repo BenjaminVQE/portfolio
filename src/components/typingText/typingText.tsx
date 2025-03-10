944e75d0328ddffb47ca0f2a23759a02662c5712
Ben <vaique.benjamin@gmail.com>
2025-03-10 11:23:43 +0100

    Remove "alternance"

--- src/components/typingText/typingText.tsx
+++ src/components/typingText/typingText.tsx
@@ -3,14 +3,7 @@ import { TypeAnimation } from "react-type-animation";
 function TypingText() {
   return (
     <TypeAnimation
-      sequence={[
-        "Développeur web Full stack",
-        2000,
-        "Etudiant",
-        1000,
-        "Recherche une alternance",
-        2000,
-      ]}
+      sequence={["Développeur web Full stack", 2000, "Etudiant", 2000]}
       wrapper="span"
       speed={50}
       style={{
